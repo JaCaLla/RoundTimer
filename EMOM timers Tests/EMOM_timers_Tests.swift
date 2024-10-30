03d254a93727ad6587f7c8aa15fc78f26a37c6b5
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-10-30 15:26:53 +0100

    Test runs on command line, from both targets

---
+++ EMOM timers Tests/EMOM_timers_Tests.swift
@@ -0,0 +1,16 @@
+//
+//  EMOM_timers_Tests.swift
+//  EMOM timers Tests
+//
+//  Created by Javier Calatrava on 30/10/24.
+//
+
+import Testing
+
+struct EMOM_timers_Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
--- EMOM timers Watch AppTests/EMOM_timers_Watch_AppTests.swift
+++ EMOM timers Watch AppTests/EMOM_timers_Watch_AppTests.swift
@@ -1,14 +1,13 @@
 //
-//  EMOM_timersTests.swift
-//  EMOM timersTests
+//  EMOM_timers_Watch_AppTests.swift
+//  EMOM timers Watch AppTests
 //
-//  Created by Javier Calatrava on 26/10/24.
+//  Created by Javier Calatrava on 30/10/24.
 //
 
 import Testing
-@testable import EMOM_timers
 
-struct EMOM_timersTests {
+struct EMOM_timers_Watch_AppTests {
 
     @Test func example() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.

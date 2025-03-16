7f98bad5ef23b71d43da4286d920e07a8c0586dc
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2025-03-16 16:38:13 +0100

    Set swift language to 6.0

--- EMOM timers Watch App/Views/EMOM_timersApp.swift
+++ EMOM timers Watch App/Views/EMOM_timersApp.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import AVFoundation
 
 @main
-struct EMOM_TimersWatchApp: App {
+struct EMOMTimersWatchApp: App {
     var body: some Scene {
         WindowGroup {
             #if false
